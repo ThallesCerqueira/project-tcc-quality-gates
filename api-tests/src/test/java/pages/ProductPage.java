655cdbbfe67c885e3c5c37275e22f272de946e2f
ThallesCerqueira <thalles.cerqueira@cepedi.org.br>
2026-09-24 17:55:58 -0300

    test: Criação de testes caminho feliz para Produtos

--- api-tests/src/test/java/pages/ProductPage.java
+++ api-tests/src/test/java/pages/ProductPage.java
@@ -1,4 +1,98 @@
 package pages;
 
+import com.github.javafaker.Faker;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import static io.restassured.RestAssured.given;
+
 public class ProductPage {
+
+    private final Faker faker = new Faker(new Locale("pt-Br"));
+    private static final String ENDPOINT = "/api/produtos";
+    private Map<String, Object> payloadProduct;
+
+    private final String nomeProduto = faker.commerce().productName();
+    private final String descricaoProduto = faker.lorem().sentence();
+    private final float precoProduto = (float) faker.number().randomDouble(2, 10, 5000);
+    private final int quantidadeProduto = faker.number().numberBetween(1, 100);
+    private final String categoriaProduto = faker.lorem().word();
+
+    public Response postProduct() {
+
+        return
+                given()
+                        .contentType(ContentType.JSON)
+                        .body(setParamsProduct())
+                        .when()
+                        .post(ENDPOINT)
+                        .then()
+                        .extract().response()
+                ;
+
+    }
+
+    public Response putProduct(int productId) {
+
+        payloadProduct.put("nome", nomeProduto + " Editado");
+
+        return
+                given()
+                        .contentType(ContentType.JSON)
+                        .body(payloadProduct)
+                        .when()
+                        .put(ENDPOINT + "/" + productId)
+                        .then()
+                        .extract().response()
+                ;
+
+    }
+
+    public Response deleteProductId(int productId) {
+
+        return
+                given()
+                        .when()
+                        .delete(ENDPOINT + "/" + productId)
+                        .then()
+                        .extract().response()
+                ;
+
+    }
+
+    public Response getAllProducts() {
+
+        return given()
+                .when()
+                .get(ENDPOINT)
+                .then()
+                .extract().response();
+    }
+
+    public Response getByProductId(int productId) {
+        return given()
+                .when()
+                .get(ENDPOINT + "/" + productId)
+                .then()
+                .extract()
+                .response();
+    }
+
+    private Map<String, Object> setParamsProduct() {
+
+        payloadProduct = new HashMap<>();
+
+        payloadProduct.put("nome", nomeProduto);
+        payloadProduct.put("descricao", descricaoProduto);
+        payloadProduct.put("preco", precoProduto);
+        payloadProduct.put("quantidade", quantidadeProduto);
+        payloadProduct.put("categoria", categoriaProduto);
+
+        return payloadProduct;
+
+    }
+
 }
--- api-tests/src/test/java/tests/ProductTest.java
+++ api-tests/src/test/java/tests/ProductTest.java
@@ -1,8 +1,81 @@
-import org.junit.jupiter.api.Test;
+package tests;
 
-public class ProductTest {
+import io.restassured.response.Response;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pages.ProductPage;
+import utils.BaseTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ProductTest extends BaseTest {
+
+    private final ProductPage productPage = new ProductPage();
+    private Response response;
+    private int productId;
+    private boolean shouldDelete;
+
+    @BeforeEach
+    public void setup() {
+
+        shouldDelete = true;
+        response = productPage.postProduct();
+        productId = response.path("id");
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+
+        if(shouldDelete) {
+            productPage.deleteProductId(productId);
+
+        }
+
+    }
 
     @Test
-    public void 
+    public void postProductTest() {
+
+        assertEquals(201, response.statusCode());
+
+    }
+
+    @Test
+    public void putProductTest() {
+
+        response = productPage.putProduct(productId);
+        assertEquals(200, response.statusCode());
+
+    }
+
+    @Test
+    public void deleteProductIdTest() {
+
+        shouldDelete = false;
+        response = productPage.deleteProductId(productId);
+        assertEquals(204, response.statusCode());
+
+    }
+
+    @Test
+    public void getAllProductsTest() {
+
+        response = productPage.getAllProducts();
+        assertEquals(200, response.statusCode());
+        assertThat(response.jsonPath().getList("$"), is(not(empty())));
+
+    }
+
+    @Test
+    public void getByProductIdTest() {
+
+        response = productPage.getByProductId(productId);
+        assertEquals(200, response.statusCode());
+
+    }
 
 }
